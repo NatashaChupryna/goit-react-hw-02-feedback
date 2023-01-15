0a4a3e7b53b7b647690aec4d6820998a3e1b944a
Natasha <natkamonte1992@gmail.com>
2023-01-15 22:40:12 +0200

    .

--- src/components/FeedbackOptions/FeedbackOptions.styled.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.styled.jsx
@@ -11,9 +11,14 @@ export const Button = styled.button`
     padding: 15px 25px;
     border-radius: 10px;
     border: none;
-    background-color: #516aed;
+    background-color: #a1a3ab;
     color: white;
     text-transform: uppercase;
     font-weight: 900;
     cursor: pointer;
+    transition: all 300ms;
+
+    :hover{
+        background-color :#516aed;
+    }
 `
