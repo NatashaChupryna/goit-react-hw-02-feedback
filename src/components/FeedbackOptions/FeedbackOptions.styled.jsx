ead005ef29e6ac1960a243b7a184eccbd4562926
Natasha <natkamonte1992@gmail.com>
2023-01-15 21:53:05 +0200

    feedback

---
+++ src/components/FeedbackOptions/FeedbackOptions.styled.jsx
@@ -0,0 +1,19 @@
+import styled from '@emotion/styled';
+
+export const FeedbackList = styled.ul`
+display: flex;
+gap: 30px;
+list-style: none;
+justify-content: flex-start;
+padding: 0;
+`
+export const Button = styled.button`
+    padding: 15px 25px;
+    border-radius: 10px;
+    border: none;
+    background-color: #516aed;
+    color: white;
+    text-transform: uppercase;
+    font-weight: 900;
+    cursor: pointer;
+`
